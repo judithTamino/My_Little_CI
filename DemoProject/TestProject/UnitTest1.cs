017c299c39ba1e8a436897966a1ee78492d797f1
connitech2020 <yosef.tagawi1@gmail.com>
2022-06-12 16:03:57 +0300

    add Nunit Tests+ update CIApp

--- DemoProject/TestProject/UnitTest1.cs
+++ DemoProject/TestProject/UnitTest1.cs
@@ -1,16 +1,38 @@
+using DemoProject;
+
 namespace TestProject
 {
+
     public class Tests
     {
+        public Calc calc; 
+
         [SetUp]
         public void Setup()
         {
+            calc = new Calc();
         }
+
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+
+            int ret = calc.Check(1, 1);
+
+            Assert.That(2,Is.EqualTo(ret));
         }
+
+        public void Test2()
+        {
+
+            int ret = calc.Check(5, 7);
+
+            Assert.That(12, Is.EqualTo(ret));
+        }
+
     }
+
+
+
 }
--- MyLittelCI/CIApp/Program.cs
+++ MyLittelCI/CIApp/Program.cs
@@ -3,13 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Permissions;
+using System.Diagnostics;
 
 namespace CIApp
 {
     internal class Program
     {
+
+        private static string projectPath = @"C:\Users\yosef\source\repos\My_Little_CI\DemoProject\";
+
+
+
+
+
+
         static void Main(string[] args)
         {
+            Watcher();
+        }
+
+
+
+
+
+        
+        private static void Watcher()
+        {
+            using (FileSystemWatcher watcher = new FileSystemWatcher())
+            {
+
+                watcher.Path = projectPath;
+                watcher.NotifyFilter = NotifyFilters.LastWrite
+                    | NotifyFilters.LastAccess
+                    | NotifyFilters.FileName
+                    | NotifyFilters.DirectoryName
+                    | NotifyFilters.Size;
+
+                watcher.Filter = "*.*";// can use *.txt for only text files
+
+                watcher.Changed += OnChanged;
+                watcher.Created += OnChanged;
+                //watcher.Changed += MSBuild;
+
+
+
+
+
+                watcher.EnableRaisingEvents = true;
+
+                Console.ReadKey(); //continue until user enters a key
+
+            }
+        }
+        private static void MSBuild(object source, FileSystemEventArgs e)
+        {
+            string pathToBuildPro = projectPath;
+            Process.Start(@"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe", pathToBuildPro);
+
+        }
+        private static void WriteLine(string line)
+        {
+            File.AppendAllText(@"C:\Users\yosef\OneDrive\Desktop\Demo\Change.txt", Environment.NewLine + line);
+            Console.WriteLine(@"C:\Users\yosef\OneDrive\Desktop\Demo\Change.txt", Environment.NewLine + line);
+
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            WriteLine("Path:" + e.FullPath + "Type:" + e.ChangeType + ",Date:" + DateTime.Now);
+
+            string pathToBuildSolution = @"C:\Users\yosef\source\repos\My_Little_CI\DemoProject\DemoProject.sln";
+            Process.Start(@"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe", pathToBuildSolution);
+        }
+
+        private static void OnRenamed(object source, FileSystemEventArgs e)
+        {
+            WriteLine("The name renamed to" + e.Name + "," + DateTime.Now);
         }
     }
 }
+
