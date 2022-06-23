dd9968557c5e092b97ce9f015237922ece73133b
judithTamino <55831729+judithTamino@users.noreply.github.com>
2022-06-23 02:32:12 -0700

    API File update

--- Program.cs
+++ Program.cs
@@ -1,18 +1,1 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DemoProject
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Calc calc = new Calc();
-            string print = calc.Check(15, 15).ToString();
-            Console.WriteLine(print);
-        }
-    }
-}
+Hello Universe! 23/06/2022 09:32:12
